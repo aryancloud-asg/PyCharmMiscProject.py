96ee5f5961e70d4370a19807002527d27dc1662b
aryancloud-asg <aryancloud32@gmail.com>
2025-09-25 14:30:33 +0530

    Initial commit

---
+++ as.py
@@ -0,0 +1,5 @@
+first_name=(input("enter the first name:"))
+last_name=(input("enter the last name:"))
+full_name= first_name +" "+ last_name
+
+print(f"\nhello, {full_name}! welcome to python program.")
---
+++ assigement 1.py
@@ -0,0 +1,10 @@
+
+a=int(input('entera the first number:'))
+b=int(input('enter the second number:'))
+
+
+
+print('addition.',a+b)
+print('subtraction.',a-b)
+print('multiplication.',a*b)
+print('division.',a/b)
